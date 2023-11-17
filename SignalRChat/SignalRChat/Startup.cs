01da16724b3ea4de821442f89a0c0737b3c62fb8
mariogz86 <99359898+mariogz86@users.noreply.github.com>
2023-11-17 15:02:54 -0600

    hh

--- SignalRChat/SignalRChat/Startup.cs
+++ SignalRChat/SignalRChat/Startup.cs
@@ -1,5 +1,7 @@
 ﻿using Owin;
 using Microsoft.Owin;
+using System.Reflection;
+
 [assembly: OwinStartup(typeof(SignalRChat.Startup))]
 namespace SignalRChat
 {
@@ -7,8 +9,11 @@ namespace SignalRChat
     {
         public void Configuration(IAppBuilder app)
         {
-            // Any connection or hub wire up and configuration should go here
-            app.MapSignalR();
-        }
+			 
+			// Any connection or hub wire up and configuration should go here
+			app.MapSignalR();
+
+			 
+		}
     }
 }
